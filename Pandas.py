28453435e02db60a09bc062496ea743c2b11e49b
Haroldo <hmetzker@gmail.com>
2022-04-03 01:33:18 -0300

    Integração Pandas x Excel

--- Pandas.py
+++ Pandas.py
@@ -1,7 +1,11 @@
 import pandas as pd
+#from openpyxl import Workbook, load_workbook
 
 p_csv = pd.read_csv('BD.csv', sep=';')
 print(p_csv)
 
 p_xlsx = pd.read_excel('BD.xlsx')
 print(p_xlsx)
+
+#p_xlsx = load_workbook('BD.xlsx')
+#print(p_xlsx)
