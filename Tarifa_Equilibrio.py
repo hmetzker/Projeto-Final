a5f4059e8210a8b8da33c6e9e45e30c2b8ce775b
bafurtado <furtadobb@gmail.com>
2022-04-05 22:29:55 -0300

    primeiras sugestões projeto

--- Tarifa_Equilibrio.py
+++ Tarifa_Equilibrio.py
@@ -1,74 +1,67 @@
 import pandas as pd
 
-class Calcula_tarifa():
+
+class CalculaTarifa:
 
     def __init__(self, tarifa_vigente, perda_custo, df):
         self.tarifa_vigente = tarifa_vigente
         self.perda_custo = perda_custo
         self.df = df
+        self.total_2019_km = 0
+        self.total_2019_pax = 0
+        self.total_2020_km = 0
+        self.total_2020_pax = 0
+        self.tarifa_equilibrio = 0
 
     def soma_excel(self):
-        global total_2019_km
-        global total_2019_pax
-        global total_2020_km
-        global total_2020_pax
-
-        total_2019_km = 0
-        total_2019_pax = 0
-        total_2020_km = 0
-        total_2020_pax = 0
-
         for i in range(self.df['ano'].count()):
             if self.df['ano'].values[i] == 2019:
-                total_2019_km = total_2019_km + self.df['km coberto'].values[i]
-                total_2019_pax = total_2019_pax + self.df['pax pagantes'].values[i]
+                self.total_2019_km = self.total_2019_km + self.df['km coberto'].values[i]
+                self.total_2019_pax = self.total_2019_pax + self.df['pax pagantes'].values[i]
             else:
-                total_2020_km = total_2020_km + self.df['km coberto'].values[i]
-                total_2020_pax = total_2020_pax + self.df['pax pagantes'].values[i]
+                self.total_2020_km = self.total_2020_km + self.df['km coberto'].values[i]
+                self.total_2020_pax = self.total_2020_pax + self.df['pax pagantes'].values[i]
+        return self.total_2019_km, self.total_2019_pax, self.total_2020_km, self.total_2020_pax
 
+    def calcula_ipk_2019(self):
+        return self.total_2019_pax / self.total_2019_km
 
-
-        return total_2019_km, total_2019_pax, total_2020_km, total_2020_pax
+    def calcula_ipk_2020(self):
+        return self.total_2020_pax / self.total_2020_km
 
     def tarifa_equivalente(self):
-        global ipk_2019
-        global ipk_2020
-        global tarifa_equilibrio
+        # diferenca_km = total_2020_km / total_2019_km
+        # diferenca_pax = total_2020_pax / total_2019_pax
 
-        ipk_2019 = total_2019_pax / total_2019_km
-        ipk_2020 = total_2020_pax / total_2020_km
-
-        diferenca_km = total_2020_km / total_2019_km
-        diferenca_pax = total_2020_pax / total_2019_pax
-
-        custo_atual = self.tarifa_vigente * ipk_2019
+        custo_atual = self.tarifa_vigente * self.calcula_ipk_2019()
         custo_novo = custo_atual * (1 + self.perda_custo)
 
-        tarifa_equilibrio = custo_novo / ipk_2020
+        self.tarifa_equilibrio = custo_novo / self.calcula_ipk_2020()
 
-        return tarifa_equilibrio, ipk_2019, ipk_2020
+        return self.tarifa_equilibrio, self.calcula_ipk_2019(), self.calcula_ipk_2020()
+
 
 if __name__ == '__main__':
-    tarifa_vigente = 4.05
+    tarif_vig = 4.05
     perda_custo = -3.0549 / 100
 
     bd_xlsx = pd.read_excel('BD.xlsx')
     df = pd.DataFrame(bd_xlsx)
 
-    ct = Calcula_tarifa(tarifa_vigente, perda_custo, df)
+    ct = CalculaTarifa(tarif_vig, perda_custo, df)
 
     ct.soma_excel()
-    print()
-    print(f'KM 2019 = {total_2019_km}')
-    print(f'KM 2020 = {total_2020_km}')
-    print()
-    print(f'PAX 2019 = {total_2019_pax}')
-    print(f'PAX 2020 = {total_2020_pax}')
-
-    te = ct.tarifa_equivalente()
-
-    print()
-    print(f'IPK 2019 = {ipk_2019}')
-    print(f'IPK 2020 = {ipk_2020}')
-    print()
-    print(f'TARIFA DE EQUILÍBRIO = {tarifa_equilibrio}')
+    # print()
+    # print(f'KM 2019 = {total_2019_km}')
+    # print(f'KM 2020 = {total_2020_km}')
+    # print()
+    # print(f'PAX 2019 = {total_2019_pax}')
+    # print(f'PAX 2020 = {total_2020_pax}')
+    #
+    # te = ct.tarifa_equivalente()
+    #
+    # print()
+    # print(f'IPK 2019 = {ipk_2019}')
+    # print(f'IPK 2020 = {ipk_2020}')
+    # print()
+    # print(f'TARIFA DE EQUILÍBRIO = {tarifa_equilibrio}')
