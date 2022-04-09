f466d4043da72f0dc5a4dfc3642e003d55214ae6
Haroldo <hmetzker@gmail.com>
2022-04-09 20:40:03 -0300

    Projeto final do curso Python do IPEA v2 (atualizado)

--- Tarifa_Equilibrio.py
+++ Tarifa_Equilibrio.py
@@ -5,9 +5,9 @@ import numpy as np
 class CalculaTarifa:
 
     def __init__(self, ano_atipico, tarifa_vigente, perda_custo, df):
-        self.tarifa_vigente = tarifa_vigente
-        self.ano_atipico = ano_atipico
-        self.perda_custo = perda_custo
+        self.tarifa_vigente = tarifa_vigente    # tarifa praticada vigente
+        self.ano_atipico = ano_atipico          # ano de queda de oferta/ demanda
+        self.perda_custo = perda_custo          # perda pela retirada de veículos da frota de ônibus
         self.df = df
         self.total_ano_anterior_km = 0
         self.total_ano_anterior_pax = 0
@@ -15,6 +15,7 @@ class CalculaTarifa:
         self.total_ano_atipico_pax = 0
         self.tarifa_equilibrio = 0
 
+    # totais do ano atípico e do ano de referência (oferta/ demanda de acordo com o planejamento)
     def soma_excel(self):
         for i in range(self.df['ano'].count()):
             if self.df['ano'].values[i] == int(self.ano_atipico) - 1:
@@ -25,13 +26,16 @@ class CalculaTarifa:
                 self.total_ano_atipico_pax = self.total_ano_atipico_pax + self.df['pax pagantes'].values[i]
         return self.total_ano_anterior_km, self.total_ano_anterior_pax, self.total_ano_atipico_km, self.total_ano_atipico_pax
 
+    # calculo do IPK de pax pagantes do ano de referência (oferta/ demanda de acordo com o planejamento)
     def calcula_ipk_ano_anterior(self):
         return self.total_ano_anterior_pax / self.total_ano_anterior_km
 
+    # calculo do IPK de pax pagantes do ano atípico (ano de queda de oferta/ demanda)
     def calcula_ipk_ano_atipico(self):
         return self.total_ano_atipico_pax / self.total_ano_atipico_km
 
-    def tarifa_equivalente(self):
+    # tarifa calculada em função da queda de oferta/ demanda (manutenção do equilíbrio econômico-financeiro)
+    def tarifa_equilibrada(self):
         custo_atual = np.float64(self.tarifa_vigente) * self.calcula_ipk_ano_anterior()
         custo_novo = custo_atual * (1 + self.perda_custo)
         self.tarifa_equilibrio = custo_novo / self.calcula_ipk_ano_atipico()
@@ -39,27 +43,27 @@ class CalculaTarifa:
 
 
 if __name__ == '__main__':
-    ano_atp = input("ANO ATÍPICO = ")
+    ano_atip = input("ANO ATÍPICO = ")
     tarifa_vig = input("TARIFA VIGENTE = ")
     perda_oferta = -3.0549 / 100
     bd_xlsx = pd.read_excel('BD.xlsx')
     d_fr = pd.DataFrame(bd_xlsx)
-    ct = CalculaTarifa(ano_atp, tarifa_vig, perda_oferta, d_fr)
+    ct = CalculaTarifa(ano_atip, tarifa_vig, perda_oferta, d_fr)
     total = ct.soma_excel()
     for i in range(0, len(total), 2):
-        saida_km = f'KM {ano_atp} = '
-        saida_pax = f'PAX {ano_atp} = '
+        saida_km = f'KM {ano_atip} = '
+        saida_pax = f'PAX {ano_atip} = '
         if i == 0:
-            saida_km = f'KM {int(ano_atp) - 1} = '
-            saida_pax = f'PAX {int(ano_atp) - 1} = '
+            saida_km = f'KM {int(ano_atip) - 1} = '
+            saida_pax = f'PAX {int(ano_atip) - 1} = '
         print(f'{saida_km}{total[i]}')
         print(f'{saida_pax}{total[i + 1]}')
 
-    t_eq = ct.tarifa_equivalente()
+    t_eq = ct.tarifa_equilibrada()
     for i in range(len(t_eq)):
-        saida_te = f'IPK {ano_atp} = '
+        saida_te = f'IPK {ano_atip} = '
         if i == 0:
-            saida_te = f'IPK {int(ano_atp) - 1} = '
+            saida_te = f'IPK {int(ano_atip) - 1} = '
         elif i == 2:
             saida_te = 'TARIFA DE EQUILÍBRIO = '
         print(f'{saida_te}{t_eq[i]}')
---
+++ Tarifa_Equilibrio_v2.py
@@ -0,0 +1,93 @@
+# calcula a tarifa de equilíbrio em função da variação (perda ou ganho) entre dois períodos distintos
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class CalculaTarifa:
+
+    def __init__(self, ano_atipico, ano_referencia, tarifa_vigente, perda_custo, df1, df2):
+# variáveis de entrada
+        self.tarifa_vigente = tarifa_vigente                # tarifa praticada vigente
+        self.ano_atipico = ano_atipico                      # ano de queda de oferta de viagens/ demanda de pax
+        self.ano_referencia = ano_referencia                # ano de referência de oferta de viagens/ demanda de pax
+        self.perda_custo = np.float64(perda_custo) / 100    # perda pela retirada de veículos da frota de ônibus
+        self.df1 = df1                                      # dataFrame referente ao ano atípico
+        self.df2 = df2                                      # dataFrame referente ao ano de referência
+# variáveis de saída
+        self.total_ano_anterior_km = 0
+        self.total_ano_anterior_pax = 0
+        self.total_ano_atipico_km = 0
+        self.total_ano_atipico_pax = 0
+        self.tarifa_equilibrio = 0
+
+# totais do ano atípico e do ano de referência (oferta/ demanda de pax de acordo com o planejamento)
+    def soma_excel(self):
+        self.total_ano_anterior_km = d_fr2['km coberto'].sum()
+        self.total_ano_atipico_km = d_fr1['km coberto'].sum()
+        self.total_ano_anterior_pax = d_fr2['pax pagantes'].sum()
+        self.total_ano_atipico_pax = d_fr1['pax pagantes'].sum()
+        return self.total_ano_anterior_km, self.total_ano_anterior_pax, self.total_ano_atipico_km, self.total_ano_atipico_pax
+
+# calculo do IPK de pax pagantes do ano de referência (oferta/ demanda de pax de acordo com o planejamento)
+    def calcula_ipk_ano_anterior(self):
+        return self.total_ano_anterior_pax / self.total_ano_anterior_km
+
+# calculo do IPK de pax pagantes do ano atípico (ano de queda de oferta/ demanda de pax)
+    def calcula_ipk_ano_atipico(self):
+        return self.total_ano_atipico_pax / self.total_ano_atipico_km
+
+# tarifa calculada em função da queda de oferta/ demanda de pax (manutenção do equilíbrio econômico-financeiro)
+    def tarifa_equilibrada(self):
+        custo_atual = np.float64(self.tarifa_vigente) * self.calcula_ipk_ano_anterior()
+        custo_novo = custo_atual * (1 - (self.total_ano_atipico_km / self.total_ano_anterior_km * self.perda_custo))
+        self.tarifa_equilibrio = custo_novo / self.calcula_ipk_ano_atipico()
+        return self.calcula_ipk_ano_anterior(), self.calcula_ipk_ano_atipico(), self.tarifa_equilibrio
+
+
+if __name__ == '__main__':
+    ano_atip = input("ANO ATÍPICO = ")
+    ano_ref = input("ANO DE REFERÊNCIA = ")
+    while ano_atip <= ano_ref:
+        print()
+        print('Ano atípico deve ser maior do que o ano de referência...')
+        print()
+        ano_atip = input("ANO ATÍPICO = ")
+        ano_ref = input("ANO DE REFERÊNCIA = ")
+    else:
+        perda_oferta = input("DIMINUIÇÃO DE CUSTO OPERACIONAL (%) = ")
+        tarifa_vig = input("TARIFA VIGENTE = ")
+        print()
+
+# arquivo em Excel com planilhas separadas para cada ano de 2015 até 2021
+# planilha_1 guarda as informações do ano atípico (queda de oferta/ demanda de pax)
+# planilha_2 guarda as informações do ano de referência quanto a oferta/ demanda de pax
+
+        planilha_1 = pd.read_excel('BD2.xlsx', sheet_name = ano_atip, index_col = 0)
+        planilha_2 = pd.read_excel('BD2.xlsx', sheet_name = ano_ref, index_col = 0)
+        d_fr1 = pd.DataFrame(planilha_1)
+        d_fr2 = pd.DataFrame(planilha_2)
+        ct = CalculaTarifa(ano_atip, ano_ref, tarifa_vig, perda_oferta, d_fr1, d_fr2)
+        total = ct.soma_excel()
+        for i in range(0, len(total), 2):
+            saida_km = f'KM {ano_atip} = '
+            saida_pax = f'PAX {ano_atip} = '
+            if i == 0:
+                saida_km = f'KM {ano_ref} = '
+                saida_pax = f'PAX {ano_ref} = '
+            print(f'{saida_km}{total[i]}')
+            print(f'{saida_pax}{total[i + 1]}')
+        t_eq = ct.tarifa_equilibrada()
+        for i in range(len(t_eq)):
+            saida_te = f'IPK {ano_atip} = '
+            if i == 0:
+                saida_te = f'IPK {ano_ref} = '
+            elif i == 2:
+                saida_te = 'TARIFA DE EQUILÍBRIO = '
+            print(f'{saida_te}{t_eq[i]}')
+# mostrando gráfico de barras
+        concData = pd.DataFrame([np.float64(tarifa_vig), np.float64(t_eq[2])])
+        concData[0].plot.barh()
+        plt.show()
