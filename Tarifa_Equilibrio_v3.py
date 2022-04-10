60f585d32f29c7cd0d770509c025476b7dd17d52
Haroldo <hmetzker@gmail.com>
2022-04-10 16:30:50 -0300

    Projeto final do curso Python do IPEA v3 (com dicionario)

--- Tarifa_Equilibrio_v3.py
+++ Tarifa_Equilibrio_v3.py
@@ -1,5 +1,11 @@
 # calcula a tarifa de equilíbrio em função da variação (perda ou ganho) entre dois períodos distintos
 
+# esta versão aprimora a importação do arquivo em Excel BD2.xlsx, ou seja:
+# 1- antes os nomes das planilhas não eram automaticos e tinhamos 2 importações de planilhas distintas do
+# BD2.xlsx para o Pandas (planilha_1, planilha_2). Utilizamos 2 DataFrames (d_fr1 e d_fr2);
+# 2- agora temos somente 1 importação da planilha BD2.xlsx e 1 DataFrame (planilha_BD2 e d_fr0), ambos com
+# base em pesquisas no Pandas através dos anos digitados (keys) e aos totais (values).
+# 3- para auxiliar no código, utilizamos conversões da planilha e do DataFrame para uma lista e um dicionário.
 
 import pandas as pd
 import numpy as np
@@ -8,12 +14,18 @@ import matplotlib.pyplot as plt
 
 class CalculaTarifa:
 
-    def __init__(self, tarifa_vigente, perda_custo, df1, df2):
+    def __init__(self, ano_atipico, ano_referencia, tarifa_vigente, perda_custo, planilha_BD2, lista_excel):
 # variáveis de entrada
+        self.ano_atipico = ano_atipico                      # ano da queda de oferta/ demanda de pax
+        self.ano_referencia = ano_referencia                # ano de referência relativo à ofeta/ demanda de pax
         self.tarifa_vigente = tarifa_vigente                # tarifa praticada vigente
         self.perda_custo = np.float64(perda_custo) / 100    # perda pela retirada de veículos da frota de ônibus
-        self.df1 = df1                                      # dataFrame referente ao ano atípico
-        self.df2 = df2                                      # dataFrame referente ao ano de referência
+        self.planilha_BD2 = planilha_BD2                    # planilha BD2 importada para o Pandas
+        self.lista_excel = lista_excel                      # corresponde BD2.xlsx convertido em lista
+
+#        self.df1 = df1                                      # dataFrame referente ao ano atípico
+#        self.df2 = df2                                      # dataFrame referente ao ano de referência
+
 # variáveis de saída
         self.total_ano_anterior_km = 0
         self.total_ano_anterior_pax = 0
@@ -23,10 +35,24 @@ class CalculaTarifa:
 
 # totais do ano atípico e do ano de referência (oferta/ demanda de pax de acordo com o planejamento)
     def soma_excel(self):
-        self.total_ano_anterior_km = self.df2['km coberto'].sum()
-        self.total_ano_atipico_km = self.df1['km coberto'].sum()
-        self.total_ano_anterior_pax = self.df2['pax pagantes'].sum()
-        self.total_ano_atipico_pax = self.df1['pax pagantes'].sum()
+        tabelaKM = {}
+        tabelaPAX = {}
+        for i in range(len(self.lista_excel)):
+            d_fr0 = pd.DataFrame(self.planilha_BD2[self.lista_excel[i]])
+            tabelaKM.update({self.lista_excel[i]: d_fr0['km coberto'].sum()})
+            tabelaPAX.update({self.lista_excel[i]: d_fr0['pax pagantes'].sum()})
+            if self.ano_atipico == self.lista_excel[i]:
+                self.total_ano_atipico_km = tabelaKM[self.lista_excel[i]]
+                self.total_ano_atipico_pax = tabelaPAX[self.lista_excel[i]]
+            if self.ano_referencia == self.lista_excel[i]:
+                self.total_ano_anterior_km = tabelaKM[self.lista_excel[i]]
+                self.total_ano_anterior_pax = tabelaPAX[self.lista_excel[i]]
+
+#        self.total_ano_anterior_km = self.df2['km coberto'].sum()
+#        self.total_ano_atipico_km = self.df1['km coberto'].sum()
+#        self.total_ano_anterior_pax = self.df2['pax pagantes'].sum()
+#        self.total_ano_atipico_pax = self.df1['pax pagantes'].sum()
+
         return self.total_ano_anterior_km, self.total_ano_anterior_pax, self.total_ano_atipico_km, self.total_ano_atipico_pax
 
 # calculo do IPK de pax pagantes do ano de referência (oferta/ demanda de pax de acordo com o planejamento)
@@ -48,6 +74,9 @@ class CalculaTarifa:
 if __name__ == '__main__':
     ano_atip = 0
     ano_ref = 1
+# importacão para o Pandas do arquivo em Excel, com planilhas separadas para cada ano
+    planilha_0 = pd.read_excel('BD2.xlsx', None)
+    lista_BD2 = list(planilha_0.keys())
     primeiraVez = False
     while ano_atip <= ano_ref:
         if primeiraVez:
@@ -59,28 +88,25 @@ if __name__ == '__main__':
         ano_atip = input("ANO ATÍPICO = ")
         ano_ref = input("ANO DE REFERÊNCIA = ")
 
-# ano atípico e/ou de referência fora dos limites (entre 2015 e 2021). Nesse caso, os valores serão corrigidos
-# pelos limites inferior (2015) e/ou superior (2021).
-# A próxima evolução será identificar automaticamente os limites inferior e superior, sem precisar fixá-los no código
+# ano(s) digitado(s) fora dos limites no Pandas. Serão corrigidos pelos limites inferior e/ou superior
 
-        if ano_atip > '2021':
-            ano_atip = '2021'
-        if ano_ref < '2015':
-            ano_ref = '2015'
+        if ano_atip > lista_BD2[-1]:
+            ano_atip = lista_BD2[-1]
+        if ano_ref < lista_BD2[0]:
+            ano_ref = lista_BD2[0]
     else:
         perda_oferta = input("DIMINUIÇÃO DE CUSTO OPERACIONAL (%) = ")
         tarifa_vig = input("TARIFA VIGENTE = ")
         print()
 
-# arquivo em Excel com planilhas separadas para cada ano de 2015 até 2021
 # planilha_1 guarda as informações do ano atípico (queda de oferta/ demanda de pax)
 # planilha_2 guarda as informações do ano de referência quanto a oferta/ demanda de pax
+#        planilha_1 = pd.read_excel('BD2.xlsx', sheet_name = ano_atip, index_col = 0)
+#        planilha_2 = pd.read_excel('BD2.xlsx', sheet_name = ano_ref, index_col = 0)
+#        d_fr1 = pd.DataFrame(planilha_1)
+#        d_fr2 = pd.DataFrame(planilha_2)
 
-        planilha_1 = pd.read_excel('BD2.xlsx', sheet_name = ano_atip, index_col = 0)
-        planilha_2 = pd.read_excel('BD2.xlsx', sheet_name = ano_ref, index_col = 0)
-        d_fr1 = pd.DataFrame(planilha_1)
-        d_fr2 = pd.DataFrame(planilha_2)
-        ct = CalculaTarifa(tarifa_vig, perda_oferta, d_fr1, d_fr2)
+        ct = CalculaTarifa(ano_atip, ano_ref, tarifa_vig, perda_oferta, planilha_0, lista_BD2)
         total = ct.soma_excel()
         for i in range(0, len(total), 2):
             saida_km = f'KM {ano_atip} = '
